df5aead759b18ea0c5fa7ef9286e7c026fb2adcd
keszod <52524513+keszod@users.noreply.github.com>
2022-12-05 13:41:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,75 +5,77 @@ import json
 import traceback
 import csv
 import requests
+import ssl
 import urllib.parse
-import undetected_chromedriver as uc
-from selenium import webdriver
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from datetime import datetime
 from bs4 import BeautifulSoup as bs
 from time import sleep
 
-def proxy_switch():
-	sub_driver = create_driver(proxy=False,headless=False)
-	sub_driver.get('https://mobileproxy.space/reload.html?proxy_key=c7aa0fba80e2aba694be5d21d2ccd011&format=json')
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+from requests.packages.urllib3.util import ssl_
 
-	return
+CIPHERS = """ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:AES256-SHA"""
 
-def get_page(url,driver,fast_exit=False,attemps=0):
-	driver.get(url)
-	sleep(5)
-	
-	soup = bs(driver.page_source,'html.parser')
-	#driver.back()
-	sleep(2)
 
-	return soup
+class TlsAdapter(HTTPAdapter):
 
+    def __init__(self, ssl_options=0, **kwargs):
+        self.ssl_options = ssl_options
+        super(TlsAdapter, self).__init__(**kwargs)
+
+    def init_poolmanager(self, *pool_args, **pool_kwargs):
+        ctx = ssl_.create_urllib3_context(ciphers=CIPHERS, cert_reqs=ssl.CERT_REQUIRED, options=self.ssl_options)
+        self.poolmanager = PoolManager(*pool_args, ssl_context=ctx, **pool_kwargs)
+
+session = requests.session()
+adapter = TlsAdapter(ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1)
+session.mount("https://", adapter)
+
+
+def get_page(url):
+	headers = get_headers()
+	print(url)
+	r = session.get(url,headers=headers)
+	test(r.text,'test.html')
+
+
+	return r.json()
+
+def get_headers():
+	with open('headers.txt','r',encoding='utf-8') as file:
+		headers = file.read()
+		headers = headers.splitlines()
+		py_headers = {}
+		for header in headers:
+			key,value = header.split(': ')
+			py_headers[key] = value
+
+		return py_headers
 
 def test(content,name):
 	with open(name,'w',encoding='utf-8') as f:
 		f.write(content)
 
-
-def parse_item():
-	soup = bs(driver.page_source,'html.parser')
-	test(driver.page_source,'test.html')
-	title = '<b>'+soup.find('h1').text+'</b>'
-	description = soup.find('div','style-item-description-pL_gy').text.replace('Описание',' ')
-	if len(description) > 800:
-		description = description[:850]+'....'
-	description = '<b>Описание</b>: \n' + description
-
-	print(description)
-	address = soup.find('span','style-item-address__string-wt61A').text.split(',')[0]
-	photo = soup.find('img','css-1qr5gpo').get('src')
-	price = soup.find('span','style-price-value-string-rWMtx').text
-
-	text = title + '\n\n' + description + '\n' + '<b>Адресс</b>: '+address+'\n\n'+'<b>Стоимость</b>: '+price
-	send_message(text,photo,driver.current_url)
-
 def parse():
 	links = []
 	with open('key_words.txt','r') as file:
 		key_words = file.read().splitlines()
-	parse_links = ['https://www.avito.ru/moskva/orgtehnika_i_rashodniki?cd=1&f=ASgCAgECAUXGmgwVeyJmcm9tIjoyMDAwMCwidG8iOjB9&s=104','https://www.avito.ru/moskva/oborudovanie_dlya_biznesa?f=ASgCAgECAUXGmgwVeyJmcm9tIjoyMDAwMCwidG8iOjB9&s=104']
+	parse_links = ['https://m.avito.ru/api/11/items?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&categoryId=99&locationId=621540&priceMin=20000&sort=date&page=1&lastStamp=1670232360&display=list&limit=25&pageId=H4sIAAAAAAAA_0q0MrSqLrYyNLRSKskvScyJT8svzUtRss60MjYyMjG3rgUEAAD__6Us-7UhAAAA&presentationType=serp','https://m.avito.ru/api/11/items?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&categoryId=40&locationId=621540&priceMin=20000&sort=date&page=1&lastStamp=1670232000&display=list&limit=25&pageId=H4sIAAAAAAAA_0q0MrSqLrYyNLRSKskvScyJT8svzUtRss60sjQxNze3rgUEAAD__2iwhx0hAAAA&presentationType=serp']
 	
 	while True:
 		for i in range(2):
-			driver.get(parse_links[i])
-			print(parse_links[i])
-			items = driver.find_elements(By.XPATH,'//div[@data-marker="item"]')
-			test(driver.page_source,'test.html')
+			data = get_page(parse_links[i])
+			test(str(json.dumps(data)),'test.html')
+			items = data['result']['items']
+			
 			for i in range(len(items)):
-				item = items[i]
-				soup = bs(item.get_attribute('innerHTML'),'html.parser')
-
-				date = soup.find('div',{"data-marker" : "item-date"}).text
-				if 'мин' in date and int(date.split()[0]) <= 20:
-					link = 'https://www.avito.ru'+soup.find('a').get('href')
-					title = soup.find('h3').text
-
+				item = items[i]['value']
+				title = item['title']
+				date = (datetime.now() - datetime.fromtimestamp(item['time'])).seconds // 60
+				print(title,date)
+				if date <= 10:
+					link = 'https://www.avito.ru'+item['uri_mweb']
 					for key_word in key_words:
 						if key_word.lower() in title.lower():
 							break
@@ -83,26 +85,21 @@ def parse():
 					if link in links:
 						continue
 
-					driver.get(link)
-					print(link)
-					content = driver.find_element(By.XPATH,'//li[@class="params-paramsList__item-appQw"]').text
+					price = item['price']
+					address = item['address']
+					photo = item['galleryItems'][0]['value']['678x678']
+					text = title + '\n\n' + '<b>Адресс</b>: '+address+'\n\n'+'<b>Стоимость</b>: '+price
+					send_message(text,photo,link)
+					
 					links.append(link)
 
-					if 'ибп' in content.lower():
-						continue
-					
-					parse_item()
-					sleep(2)
-					
-					driver.back()
-					items = driver.find_elements(By.XPATH,'//div[@data-marker="item"]')
-
 			sleep(3)
-		sleep(30)
+		sleep(60*3)
 
 def send_message(text,photo,link):
 	token = '934338853:AAHfa6yri8ktKUUtgIRZGaVgKMrn2oB_GLk'
 	chat_id = '917403306'
+	#chat_id = '618939593'
 	print(text)
 
 	keyboard = []
@@ -119,10 +116,4 @@ def send_message(text,photo,link):
 				
 
 if __name__ == '__main__':
-	options = uc.ChromeOptions()
-	options.headless=True
-	options.add_argument('--headless')
-	driver = uc.Chrome(options=options)
-	driver.implicitly_wait(30)
 	parse()
-
